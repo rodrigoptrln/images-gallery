49bec6972ab1c5e6d0ae58f889c4097a5f42799a
Rodrigo Pater <rodpater@gmail.com>
2021-11-20 07:57:09 -0300

    fix Welcome.js - class for className

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const Welcome = () => (
-  <div class="p-3 jumbotron">
-    <h1 class="display-4">Images Gallery</h1>
-    <p class="lead">
+  <div className="p-3 jumbotron">
+    <h1 className="display-4">Images Gallery</h1>
+    <p className="lead">
       This is a simple application that retrieve photos from Unsplash API. In
       order to start enter any search term in the input field.
     </p>
-    <hr class="my-4" />
+    <hr className="my-4" />
     <p>
       Unsplash é um site que prove imagens para uso gratuito e sem restrições
       comerciais e direitos de imagem, livre para o uso geral.
