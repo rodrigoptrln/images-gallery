5128d53fa7c6c9076fb865c88e893c8df0d54fab
Rodrigo Pater <rodpater@gmail.com>
2021-11-23 17:28:06 -0300

    flask api

--- api/main.py
+++ api/main.py
@@ -7,11 +7,14 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL="https://api.unsplash.com/photos/random?query=...."
 UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+DEBUG=bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Por favor, crie o aquivo .env.local e insira a chave UNSPLASH_KEY")
 
 app = Flask(__name__)
+
+app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
 def new_image():
